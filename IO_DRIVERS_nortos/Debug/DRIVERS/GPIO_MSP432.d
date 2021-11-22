83d706a1619a8f10aa65ecdd5310565bd4d24724
alfredochac <94699096+alfredochac@users.noreply.github.com>
2021-11-21 23:22:26 -0700

    nortos

---
+++ IO_DRIVERS_nortos/Debug/DRIVERS/GPIO_MSP432.d
@@ -0,0 +1,114 @@
+# FIXED
+
+DRIVERS/GPIO_MSP432.obj: ../DRIVERS/GPIO_MSP432.c
+DRIVERS/GPIO_MSP432.obj: ../DRIVERS/GPIO_MSP432.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/core_cm4.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_version.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_compiler.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_ccs.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/mpu_armv7.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h
+DRIVERS/GPIO_MSP432.obj: ../DRIVERS/system.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/types.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/endian.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/select.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_sigset.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_timeval.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/timespec.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_timespec.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stddef.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdbool.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdio.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdarg.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdlib.h
+DRIVERS/GPIO_MSP432.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/string.h
+DRIVERS/GPIO_MSP432.obj: C:/Users/intel14/workspace_v10/WORKSHOP_2021/IO_DRIVERS_nortos/Drivers/BSP.h
+DRIVERS/GPIO_MSP432.obj: C:/Users/intel14/workspace_v10/WORKSHOP_2021/IO_DRIVERS_nortos/Drivers/GPIO_MSP432.h
+
+../DRIVERS/GPIO_MSP432.c:
+
+../DRIVERS/GPIO_MSP432.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/core_cm4.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_version.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_compiler.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_ccs.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/mpu_armv7.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h:
+
+../DRIVERS/system.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/types.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/endian.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/select.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_sigset.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_timeval.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/timespec.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_timespec.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stddef.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdbool.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdio.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdarg.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdlib.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/string.h:
+
+C:/Users/intel14/workspace_v10/WORKSHOP_2021/IO_DRIVERS_nortos/Drivers/BSP.h:
+
+C:/Users/intel14/workspace_v10/WORKSHOP_2021/IO_DRIVERS_nortos/Drivers/GPIO_MSP432.h:
+
---
+++ IO_DRIVERS_nortos/Debug/main.d
@@ -0,0 +1,114 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../DRIVERS/BSP.h
+main.obj: ../Drivers/GPIO_MSP432.h
+main.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h
+main.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h
+main.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h
+main.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/core_cm4.h
+main.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_version.h
+main.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_compiler.h
+main.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_ccs.h
+main.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/mpu_armv7.h
+main.obj: C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h
+main.obj: ../Drivers/system.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/types.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/endian.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/select.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_sigset.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_timeval.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/timespec.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_timespec.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stddef.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdbool.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdio.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdarg.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdlib.h
+main.obj: C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/string.h
+main.obj: C:/Users/intel14/workspace_v10/WORKSHOP_2021/IO_DRIVERS_nortos/Drivers/BSP.h
+
+../main.c:
+
+../DRIVERS/BSP.h:
+
+../Drivers/GPIO_MSP432.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdint.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_ti_config.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/linkage.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/_stdint40.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/stdint.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/cdefs.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_types.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_types.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/_stdint.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_stdint.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp_compatibility.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/core_cm4.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_version.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_compiler.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/cmsis_ccs.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/third_party/CMSIS/Include/mpu_armv7.h:
+
+C:/ti_v10_1/simplelink_msp432p4_sdk_3_40_01_02/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h:
+
+../Drivers/system.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/types.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/machine/endian.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/select.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_sigset.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_timeval.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/timespec.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/sys/_timespec.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stddef.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdbool.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdio.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdarg.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/stdlib.h:
+
+C:/ti_v10_1/ccs/tools/compiler/ti-cgt-arm_20.2.1.LTS/include/string.h:
+
+C:/Users/intel14/workspace_v10/WORKSHOP_2021/IO_DRIVERS_nortos/Drivers/BSP.h:
+
